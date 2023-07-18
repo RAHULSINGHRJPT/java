1c374d64aead76c8ad57b67e04c65bb998b51f93
Rahul Singh Rajput <108928913+RAHULSINGHRJPT@users.noreply.github.com>
2023-07-18 09:55:18 +0530

    Update matrixmul.java

--- matrixmul.java
+++ matrixmul.java
@@ -1,26 +1,61 @@
+import java.util.*;
+public class matrixmul {
+	static void printMatrix(int M[][], int row, int col) {
+		for (int i = 0; i < row; i++) {
+			for (int j = 0; j < col; j++) {
+				System.out.print(M[i][j] + " ");
+			}
+			System.out.println();
+		}
+	}
 
-//write a java program to multiply two matrices
+	static void Multiply(int row1, int col1, int arr1[][], int row2, int col2, int arr2[][]) {
+		System.out.println("First Matrix:");
+		printMatrix(arr1, row1, col1);
+		System.out.println("Second Matrix:");
+		printMatrix(arr2, row2, col2);
+		int arr3[][] = new int[row1][col2];
+		for (int i = 0; i < row1; i++) {
+			for (int j = 0; j < col2; j++) {
+				for(int k=0; k<row2; k++) {
+                    arr3[i][j] += arr1[i][j] * arr2[i][j];
+                }
+			}
+		}
+		System.out.println("Resultant Matrix:");
+		printMatrix(arr3, row1, col2);
+	}
 
-public class matrixmul{  
-    public static void main(String args[]){  
-    //creating two matrices    
-    int a[][]={{1,1,1},{2,2,2},{3,3,3}};    
-    int b[][]={{1,1,1},{2,2,2},{3,3,3}};    
-        
-    //creating another matrix to store the multiplication of two matrices    
-    int c[][]=new int[3][3];  //3 rows and 3 columns  
-        
-    //multiplying and printing multiplication of 2 matrices    
-    for(int i=0;i<3;i++){    
-    for(int j=0;j<3;j++){    
-    c[i][j]=0;      
-    for(int k=0;k<3;k++)      
-    {      
-    c[i][j]+=a[i][k]*b[k][j];      
-    }//end of k loop  
-    System.out.print(c[i][j]+" ");  //printing matrix element  
-    }//end of j loop  
-    System.out.println();//new line    
-    }    
-    }}  
-
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the number of rows in first matrix");
+		int row1 = sc.nextInt();
+		System.out.println("Enter the number of columns in first matrix");
+		int col1 = sc.nextInt();
+		System.out.println("Enter the number of rows in second matrix");
+		int row2 = sc.nextInt();
+		System.out.println("Enter the number of columns in second matrix");
+		int col2 = sc.nextInt();
+		if (row2 != col1) {
+			System.out.println("Multiplication Not Possible");
+		}
+		else {
+			int arr1[][] = new int[row1][col1];
+			int arr2[][] = new int[row2][col2];
+			System.out.println("Enter the elements of first matrix:");
+			for (int i = 0; i < row1; i++) {
+				for (int j = 0; j < col1; j++) {
+					arr1[i][j] = sc.nextInt();
+				}
+			}
+			System.out.println("");
+			System.out.println("Enter the elements of second matrix:");
+			for (int i = 0; i < row2; i++) {
+				for (int j = 0; j < col2; j++) {
+					arr2[i][j] = sc.nextInt();
+				}
+			}
+			Multiply(row1, col1, arr1, row2, col2, arr2);
+		}
+	}
+}
