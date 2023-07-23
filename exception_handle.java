3272a85284fe08ee1d67729657d21f6e6248a3e9
Rahul Singh Rajput <108928913+RAHULSINGHRJPT@users.noreply.github.com>
2023-07-23 23:58:46 +0530

    Update exception_handle.java

--- exception_handle.java
+++ exception_handle.java
@@ -1,6 +1,21 @@
 
 // design a java code to implement exception handeling
 
-public class exception_handle {
+class exception_handle{
+    static int add(int a, int b){
+        return a+b;
+    }
+    static int add(int a, int b, int c){
+        return a+b+c;
+    }
+
     
 }
+class Overload{
+    public static void main(String[] args) {
+        System.out.println(Addition.add(12,18));
+        System.out.println(Addition.add(12,18,8 ));
+    }
+}
+
+
