36ae2760541b28ca62ddda1c7a35f0564ae2add1
Rahul Singh Rajput <108928913+RAHULSINGHRJPT@users.noreply.github.com>
2023-07-24 00:25:13 +0530

    Update sumtillsingel.java

--- sumtillsingel.java
+++ sumtillsingel.java
@@ -2,10 +2,25 @@
 //write a java program to take 2 digit input from user and add the corresponding once and tens placed numbers till it becomes a single digit number
 
 import java.util.*;
-public class sumtillsingel {
-    public static void main(String args[]){
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter a 2 digit number : ");
-        int n = sc.nextInt();
+public class Digit{
+    public static void calc(int num) {
+        int sum=0;
+        while(num>0 || sum>9)
+        {
+            if(num==0)
+            {
+                num = sum;
+                sum=0;
+            }
+            sum+=num%10;
+            num/=10;
+        }
+        System.out.print(sum);
     }
-}
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number");
+        int n = sc.nextInt();
+        calc(n);
+    }
+} 
